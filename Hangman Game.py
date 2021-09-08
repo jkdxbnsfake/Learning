72a1f005b3a035a83f6a80f4566e5cc4e2a5f222
Jkdxbns <90069786+Jkdxbns@users.noreply.github.com>
2021-09-09 03:56:18 +0530

    Add files via upload

---
+++ Hangman Game.py
@@ -0,0 +1,29 @@
+import random
+
+print("Welcome to Hangman !")
+
+movies_list = ["Inception", "The Avengers", "The Dhoom"]
+chosen_movie_name = list(str(random.choice(movies_list)).lower())
+#print(chosen_movie_name)
+print("\n")
+
+new_list =["_" for x in range(len(chosen_movie_name))]
+end_of_game = False
+
+while not end_of_game:
+    user_guess = input("What letters do you think the movie name has ?\t")
+    for n in range(0, len(chosen_movie_name)):
+        if chosen_movie_name[n] == " ":
+            new_list[n] = " "
+        elif user_guess == chosen_movie_name[n]:
+            new_list[n] = user_guess
+
+    print(f"movie name is : {(''.join(new_list)).capitalize()}")
+    print("\n")
+    if "_" in list(new_list):
+        end_of_game = False
+    elif "_" not in list(new_list):
+        end_of_game = True
+
+print("CONGRATULATIONS ,YOU HAVE WON !")
+
